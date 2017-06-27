d39fe541f7012700ae33ca55fb668d979064f26c
ManfredSteyer <manfred.steyer@gmx.net>
2017-06-27 21:29:15 +0200

    first commit

---
+++ application/src/app/app.module.ts
@@ -0,0 +1,67 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule, Http } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { FlightSearchComponent } from './flight-booking/flight-search/flight-search.component';
+import { FlightService } from './flight-booking/flight-search/flight.service';
+import { BASE_URL } from './app.tokens';
+import { CityPipe } from './shared/pipes/city.pipe';
+import { EventService } from './event.service';
+import { BasketComponent } from './basket/basket.component';
+import { AppRouterModule } from './app.routes';
+import { HomeComponent } from './home/home.component';
+import { SharedModule } from './shared/shared.module';
+
+import { OAuthModule } from 'angular-oauth2-oidc';
+
+import { AuthModule } from './auth/auth.module';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
+
+import { SampleModule } from 'my-lib';
+
+
+export function createLoader(http: Http) {
+    return new TranslateHttpLoader (http, './assets/i18n/', '.json');
+    // ./assets/i18n/de.json
+}
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+
+    AuthModule.forRoot(),
+
+    SampleModule.forRoot(),
+
+    // FlightBookingModule,
+    AppRouterModule,
+    SharedModule,
+
+    TranslateModule.forRoot({
+        loader: {
+            provide: TranslateLoader,
+            useFactory: createLoader,
+            deps: [Http]
+        }
+    }),
+    OAuthModule.forRoot()
+  ],
+  declarations: [
+    AppComponent,
+    BasketComponent,
+    HomeComponent
+  ],
+  providers: [
+    // { provide: FlightService, useClass: FlightService }
+    // FlightService
+    EventService,
+    { provide: BASE_URL, useValue: 'http://www.angular.at/api'}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
